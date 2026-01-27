686387c6fd7cce1f059d1d491dd63a623ab8ca6f
krisiwebsitemaker1234 <krisisipri2009@gmail.com>
2026-01-27 15:12:10 +0100

    test

--- superadmin/edit-teacher.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../config/functions.php';
-require_role(['superadmin']);
-// Redirect to list page - edit functionality to be implemented
-header('Location: teachers.php');
-exit();
-?>
