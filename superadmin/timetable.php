b0201d8e4b93cc4eefcc1ff4d3238936db0ee25e
krisiwebsitemaker1234 <krisisipri2009@gmail.com>
2026-01-28 15:25:07 +0100

    test

--- superadmin/timetable.php
+++ superadmin/timetable.php
@@ -164,7 +164,7 @@ try {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Timetable Management - <?php echo defined('SITE_NAME') ? SITE_NAME : 'School Management'; ?></title>
+  <title>Timetable Management - </title>
   <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
   <link rel="stylesheet" href="../assets/css/styles.min.css" />
 </head>
